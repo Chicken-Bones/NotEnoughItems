b0299cd136f2afdd06298ad33e1bdc3cf951f52a
Chicken-Bomes <mehvids@gmail.com>
2014-08-04 23:17:11 +1000

    Workaround for colouring supporters list with ANSI encoding

--- src/codechicken/nei/ClientHandler.java
+++ src/codechicken/nei/ClientHandler.java
@@ -1,7 +1,6 @@
 package codechicken.nei;
 
 import codechicken.core.ClientUtils;
-import codechicken.core.GuiModListScroll;
 import codechicken.lib.packet.PacketCustom;
 import codechicken.nei.api.API;
 import codechicken.nei.api.ItemInfo;
@@ -116,7 +115,7 @@ public class ClientHandler
     public static void load() {
         instance = new ClientHandler();
 
-        GuiModListScroll.register("NotEnoughItems");
+        ClientUtils.enhanceSupportersList("NotEnoughItems");
         PacketCustom.assignHandler(NEICPH.channel, new NEICPH());
         FMLCommonHandler.instance().bus().register(instance);
         MinecraftForge.EVENT_BUS.register(instance);
